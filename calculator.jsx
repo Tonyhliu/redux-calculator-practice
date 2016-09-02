247f131fe59dfae9d522591b9aab1545bfe6e622
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-01 20:49:45 -0700

    sets state correctly

--- calculator.jsx
+++ calculator.jsx
@@ -8,13 +8,33 @@ class Calculator extends React.Component{
       userInputOne: "",
       userInputTwo: ""
     }
+
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+  };
+
+  setNum1(e) {
+    // render empty string if no input or if backspaced
+    const userInputOne = e.target.value ? parseInt(e.target.value) : ""
+    this.setState({userInputOne})
   }
 
-  //your code here
+  setNum2(e) {
+    // render empty string if no input or if backspaced
+    const userInputTwo = e.target.value ? parseInt(e.target.value) : ""
+    this.setState({userInputTwo})
+  }
 
   render(){
+    console.log(this.state);
     return (
       <div>
+        <input type="text"
+              value={this.state.userInputOne}
+              onChange={this.setNum1}></input>
+        <input value={this.state.userInputTwo}
+              type="text"
+              onChange={this.setNum2}></input>
         <h1>{this.state.result}</h1>
       </div>
     );
