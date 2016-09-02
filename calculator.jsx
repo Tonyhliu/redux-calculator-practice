a276b2d95969519d59e5a663cc59cb09a49ff620
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-09-01 21:06:35 -0700

    simple react calculator with es6 syntax complete

--- calculator.jsx
+++ calculator.jsx
@@ -11,22 +11,57 @@ class Calculator extends React.Component{
 
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
+    this._add = this._add.bind(this);
+    this._subtract = this._subtract.bind(this);
+    this._multiply = this._multiply.bind(this);
+    this._divide = this._divide.bind(this);
+    this._clear = this._clear.bind(this);
   };
 
   setNum1(e) {
     // render empty string if no input or if backspaced
-    const userInputOne = e.target.value ? parseInt(e.target.value) : ""
-    this.setState({userInputOne})
+    const userInputOne = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({userInputOne});
   }
 
   setNum2(e) {
     // render empty string if no input or if backspaced
-    const userInputTwo = e.target.value ? parseInt(e.target.value) : ""
-    this.setState({userInputTwo})
+    const userInputTwo = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({userInputTwo});
+  }
+
+  _add(e) {
+    e.preventDefault();
+    const result = this.state.userInputOne + this.state.userInputTwo;
+    this.setState({result});
+  }
+
+  _subtract(e) {
+    e.preventDefault();
+    const result = this.state.userInputOne - this.state.userInputTwo;
+    this.setState({result});
+  }
+
+  _multiply(e) {
+    e.preventDefault();
+    const result = this.state.userInputOne * this.state.userInputTwo;
+    this.setState({result})
+  }
+
+  _divide(e) {
+    e.preventDefault();
+    const result = this.state.userInputOne / this.state.userInputTwo;
+    this.setState({result})
+  }
+
+  _clear(e) {
+    e.preventDefault();
+    const result = 0;
+    this.setState({result});
   }
 
   render(){
-    console.log(this.state);
+    // console.log(this.state);
     return (
       <div>
         <input type="text"
@@ -35,6 +70,11 @@ class Calculator extends React.Component{
         <input value={this.state.userInputTwo}
               type="text"
               onChange={this.setNum2}></input>
+            <button onClick={this._add}>+</button>
+            <button onClick={this._subtract}>-</button>
+            <button onClick={this._multiply}>*</button>
+            <button onClick={this._divide}>/</button>
+            <button onClick={this._clear}>Clear</button>
         <h1>{this.state.result}</h1>
       </div>
     );
